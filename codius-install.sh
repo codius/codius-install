3577a5e2bf2988585a6081f7d2735917bb882434
Brandon Wilson <brandon@coil.com>
2020-06-09 18:10:41 -0500

    switch back to master branch

--- codius-install.sh
+++ codius-install.sh
@@ -2,7 +2,7 @@
 # File              : codius-install.sh
 # Author            : N3TC4T <netcat.av@gmail.com>
 # Date              : 16.06.2018
-# Last Modified Date: 13.05.2020
+# Last Modified Date: 09.06.2020
 # Last Modified By  : wilsonianb <brandon@coil.com>
 # Copyright (c) 2018 N3TC4T <netcat.av@gmail.com>
 #
@@ -33,7 +33,7 @@ CURL_C="curl -SL -o"
 LOG_OUTPUT="/tmp/${0##*/}$(date +%Y-%m-%d.%H-%M)"
 CURRENT_USER="$(id -un 2>/dev/null || true)"
 BASE_DIR=$(cd "$(dirname "$0")"; pwd); cd ${BASE_DIR}
-INSTALLER_BRANCH="operator"
+INSTALLER_BRANCH="master"
 INSTALLER_URL="https://raw.githubusercontent.com/wilsonianb/codius-install/${INSTALLER_BRANCH}/codius-install.sh"
 K8S_MANIFEST_PATH="https://raw.githubusercontent.com/wilsonianb/codius-install/${INSTALLER_BRANCH}/manifests"
 ########## k3s ##########
